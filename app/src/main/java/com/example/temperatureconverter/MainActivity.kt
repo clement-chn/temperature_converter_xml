9d2884b5f9117aa7c4c617781e59c2971b571085
Clément Chouan <clement@MacBook-Air-de-Clement.local>
2024-12-02 10:34:41 +0100

    ajout d'une toolbar

--- app/src/main/java/com/example/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.temperatureconverter
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +43,47 @@ class MainActivity : AppCompatActivity() {
                     tempImage.setImageResource(R.drawable.cold)
                 }
             }
+        }
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+
+            R.id.action_search -> {
+                Toast.makeText(
+                    this, "Action pour le bouton de recherche",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+                true
+            }
+
+            R.id.action_about -> {
+                val toast = Toast.makeText(
+                    this,
+                    "Action pour les paramètres",
+                    Toast.LENGTH_SHORT
+                )
+                toast.show()
+                true
+            }
+
+            R.id.action_settings -> {
+                Toast.makeText(
+                    this, "Action pour le à propos",
+                    Toast.LENGTH_SHORT
+                ).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
